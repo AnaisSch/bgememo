adcdc55691132cac52a0a54bdba6b25dc48a47cb
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-19 11:15:13 +0100

    add gitlog graph in terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -8,6 +8,7 @@ git add <nom du fichier / dossier>                        # ajouter les modifica
 git commit -m "<message à attacher au commit>"            # sauvegardé les modifications précédemment ajoutées
 git status                                                # affiche l'état de mon projet git à cet instant
 git log                                                   # affiche l'historique des commits, pour sortir "q"
+git log -- pretty=oneline --graph                        # affiche l'historique des commits sous forme de graphe
 git restore <nom fichier / dossier>                       # remet le contenu du fichier à l'état du dernier commit
 git checkout <nom d’une branche ou hash du commit>        # déplacer la tete de lecture (HEAD) sur la branche ou le commit choisi 
 git branch <nom de la branche>                            # créé une branche en copiant la branche actuelle 
