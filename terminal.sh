b6fd1bd63971152542520fe35c2776c419c7766a
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-19 10:53:35 +0100

    add checkout and branch on git memo

--- terminal.sh
+++ terminal.sh
@@ -1,11 +1,14 @@
 #Divers 
-cd <chemin du dossier>                          # : pour se déplacer 
-node <nom du fichier>                           # : lancer un script avec node.js
+cd <chemin du dossier>                                    # : pour se déplacer 
+node <nom du fichier>                                     # : lancer un script avec node.js
 
 #GIT
-git init                                        # initialise la sauvegarde d'un projet git 
-git add <nom du fichier / dossier>              # ajouter les modifications au prochain commit
-git commit -m "<message à attacher au commit>"  # sauvegardé les modifications précédemment ajoutées
-git status                                      # affiche l'état de mon projet git à cet instant
-git log                                         # affiche l'historique des commits, pour sortir "q"
-git restore <nom fichier / dossier>             # remet le contenu du fichier à l'état du dernier commit
+git init                                                  # initialise la sauvegarde d'un projet git 
+git add <nom du fichier / dossier>                        # ajouter les modifications au prochain commit
+git commit -m "<message à attacher au commit>"            # sauvegardé les modifications précédemment ajoutées
+git status                                                # affiche l'état de mon projet git à cet instant
+git log                                                   # affiche l'historique des commits, pour sortir "q"
+git restore <nom fichier / dossier>                       # remet le contenu du fichier à l'état du dernier commit
+git checkout <nom d’une branche ou hash du commit>        # déplacer la tete de lecture (HEAD) sur la branche ou le commit choisi 
+git branch <nom de la branche>                            # créé une branche en copiant la branche actuelle 
+git branch                                                # liste les branches existantes
