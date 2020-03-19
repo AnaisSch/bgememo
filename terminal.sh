9d489cfe4292e5d509734fa2c6cc884bd98ac844
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-19 09:38:35 +0100

    add terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,10 +1,10 @@
 #Divers 
-cd <chemin du dossier>                  # : pour se déplacer 
-node <nom du fichier>                   # : lancer un script avec node.js
+cd <chemin du dossier>                          # : pour se déplacer 
+node <nom du fichier>                           # : lancer un script avec node.js
 
 #GIT
-git init                                # initialise la sauvegarde d'un projet git 
-git add <nom du fichier / dossier>      # ajouter les modifications au prochain commit
-git commit -m "<message à attacher au commit>" # sauvegardé les modifications précédemment ajoutées
-git status                              # affiche l'état de mon projet git à cet instant
-git log                                 # affiche l'historique des commits
+git init                                        # initialise la sauvegarde d'un projet git 
+git add <nom du fichier / dossier>              # ajouter les modifications au prochain commit
+git commit -m "<message à attacher au commit>"  # sauvegardé les modifications précédemment ajoutées
+git status                                      # affiche l'état de mon projet git à cet instant
+git log                                         # affiche l'historique des commits
