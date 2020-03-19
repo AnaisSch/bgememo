86c16260c2c486b9ced29647ee6bb60d6135e02b
AnaisSch <60514726+AnaisSch@users.noreply.github.com>
2020-03-19 14:37:08 +0100

    add set-upstream on terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -8,11 +8,9 @@ git add <nom du fichier / dossier>                        # ajouter les modifica
 git commit -m "<message à attacher au commit>"            # sauvegardé les modifications précédemment ajoutées
 git status                                                # affiche l'état de mon projet git à cet instant
 git log                                                   # affiche l'historique des commits, pour sortir "q"
-git log -- pretty=oneline --graph                         # affiche l'historique des commits sous forme de graphe
 git restore <nom fichier / dossier>                       # remet le contenu du fichier à l'état du dernier commit
-git checkout <nom d’une branche ou hash du commit>        # déplacer la tete de lecture (HEAD) sur la branche ou le commit choisi 
+git checkout <nom d’une branche ou hash du commit>        # déplacer la tete de lecture (HEAD) sur la branche ou le commit      choisi 
 git branch <nom de la branche>                            # créé une branche en copiant la branche actuelle 
 git branch                                                # liste les branches existantes
-git merge <nom de la branche> -m "<message du commit>"    # fusionne la branche nommée à l'intérieur de la branche actuelle, en créant un commit
-git remote                                                # liste les dépôts distants de notre projet git
-git remote add <nom du dépôt distant> <adresse du dépôt>  # ajoute un nouveau dépôt distant (origin dans 99% des cas)
+git push <nom du dépôt> <branche distante>                # envoie la branche courante sur la branche distante du dépôt choisi
+git push -u <nom du dépôt> <branche distante>             # idem, mais paramètre la branche locale pour correspondre à la       branche distante
